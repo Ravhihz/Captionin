cb3ab0067bf2290ea6097234043b8da890818db7
ravhihz <ravhi.satria@gmail.com>
2026-07-18 18:04:19 +0700

    Update robot

---
+++ app/robot.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://captionin.varstory.my.id/sitemap.xml",
+  };
+}
